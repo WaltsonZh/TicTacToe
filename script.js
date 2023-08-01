ed0f8a62a9d6c4c3cfb3b97a5514015d21d7b39f
adamaconguli <waltson2003@gmail.com>
2023-08-01 21:27:32 +0800

    algorithm: completed AI algorithm

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const board = document.querySelectorAll("td");
+const board = document.querySelectorAll('td')
 
-const crossbtn = document.getElementById("crossbtn");
-const circlebtn = document.getElementById("circlebtn");
+const crossbtn = document.getElementById('crossbtn')
+const circlebtn = document.getElementById('circlebtn')
 
-const resetbtn = document.getElementById("resetbtn");
+const resetbtn = document.getElementById('resetbtn')
 
-const cross = document.querySelectorAll(".cross");
-const circle = document.querySelectorAll(".circle");
+const cross = document.querySelectorAll('.cross')
+const circle = document.querySelectorAll('.circle')
 
 const lines = [
     [0, 1, 2],
@@ -17,247 +17,270 @@ const lines = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-];
+]
 
-let empty = new Array(9).fill(true);
-let playerX = true;
-let enabled = true;
-let rounds = 0;
+let empty = new Array(9).fill(true)
+let playerX = true
+let enabled = true
+let rounds = 0
 
-let game = new Array(9).fill(0); // 0: empty, 1: cross, -1: circle
-let finished = false;
+let game = new Array(9).fill(0) // 0: empty, 1: cross, -1: circle
+let finished = false
 
 cross.forEach((cro) => {
-    const centerX = cro.width / 2;
-    const centerY = cro.height / 2;
-    const cav = cro.getContext("2d");
-    cav.clearRect(0, 0, cro.width, cro.height);
+    const centerX = cro.width / 2
+    const centerY = cro.height / 2
+    const cav = cro.getContext('2d')
+    cav.clearRect(0, 0, cro.width, cro.height)
 
-    cav.translate(centerX, centerY);
-    cav.rotate(Math.PI / 4);
+    cav.translate(centerX, centerY)
+    cav.rotate(Math.PI / 4)
 
-    cav.strokeStyle = "hsl(210, 30%, 60%)";
-    cav.lineWidth = 10;
+    cav.strokeStyle = 'hsl(210, 30%, 60%)'
+    cav.lineWidth = 10
 
-    cav.beginPath();
-    cav.moveTo(-40, 0);
-    cav.lineTo(40, 0);
-    cav.stroke();
+    cav.beginPath()
+    cav.moveTo(-40, 0)
+    cav.lineTo(40, 0)
+    cav.stroke()
 
-    cav.beginPath();
-    cav.moveTo(0, -40);
-    cav.lineTo(0, 40);
-    cav.stroke();
-});
+    cav.beginPath()
+    cav.moveTo(0, -40)
+    cav.lineTo(0, 40)
+    cav.stroke()
+})
 
 circle.forEach((cir) => {
-    const centerX = cir.width / 2;
-    const centerY = cir.height / 2;
-    const cav = cir.getContext("2d");
-    cav.clearRect(0, 0, cir.width, cir.height);
+    const centerX = cir.width / 2
+    const centerY = cir.height / 2
+    const cav = cir.getContext('2d')
+    cav.clearRect(0, 0, cir.width, cir.height)
 
-    cav.beginPath();
-    cav.arc(centerX, centerY, 35, 0, Math.PI * 2);
-    cav.arc(centerX, centerY, 32, 0, Math.PI * 2, true);
-    cav.closePath();
+    cav.beginPath()
+    cav.arc(centerX, centerY, 35, 0, Math.PI * 2)
+    cav.arc(centerX, centerY, 32, 0, Math.PI * 2, true)
+    cav.closePath()
 
-    cav.strokeStyle = "hsl(210, 40%, 25%)";
-    cav.fillStyle = "hsl(210, 40%, 25%)";
-    cav.lineWidth = 10;
+    cav.strokeStyle = 'hsl(210, 40%, 25%)'
+    cav.fillStyle = 'hsl(210, 40%, 25%)'
+    cav.lineWidth = 10
 
-    cav.stroke();
-    cav.fill();
-});
+    cav.stroke()
+    cav.fill()
+})
 
-circlebtn.addEventListener("click", () => {
-    crossbtn.disabled = true;
-    circlebtn.disabled = true;
-    document.getElementById("hint").innerHTML = "Turn of &times;";
+circlebtn.addEventListener('click', () => {
+    crossbtn.disabled = true
+    circlebtn.disabled = true
+    document.getElementById('hint').innerHTML = 'Turn of &times;'
 
     setTimeout(() => {
-        enabled = false;
-        show(board[1], 1);
-    }, 600);
-});
+        enabled = false
+        show(board[1], 1)
+    }, 600)
+})
 
-crossbtn.addEventListener("click", () => {
-    crossbtn.disabled = true;
-    circlebtn.disabled = true;
-    document.getElementById("hint").innerHTML = "Turn of &times;";
-});
+crossbtn.addEventListener('click', () => {
+    crossbtn.disabled = true
+    circlebtn.disabled = true
+    document.getElementById('hint').innerHTML = 'Turn of &times;'
+})
 
 board.forEach((block, index) => {
-    block.addEventListener("click", () => {
-        crossbtn.disabled = true;
-        circlebtn.disabled = true;
+    block.addEventListener('click', () => {
+        crossbtn.disabled = true
+        circlebtn.disabled = true
         if (empty[index] && enabled) {
-            show(block, index);
+            show(block, index)
 
             setTimeout(() => {
-                position = algorithm();
+                position = algorithm()
                 if (position != -1) {
-                    show(board[position], position);
+                    show(board[position], position)
                 }
                 if (rounds == 9) {
-                    showResult();
+                    showResult()
                 }
-            }, 600);
+            }, 600)
         }
-    });
+    })
 
-    block.addEventListener("mouseover", () => {
+    block.addEventListener('mouseover', () => {
         if (empty[index]) {
-            blockMouseover(block);
+            blockMouseover(block)
         }
-    });
+    })
 
-    block.addEventListener("mouseleave", () => {
-        blockMouseleave(block);
-    });
-});
+    block.addEventListener('mouseleave', () => {
+        blockMouseleave(block)
+    })
+})
 
-resetbtn.addEventListener("click", () => {
+resetbtn.addEventListener('click', () => {
     board.forEach((block) => {
-        block.querySelectorAll("canvas").forEach((cav) => {
-            cav.style.display = "none";
-        });
-    });
+        block.querySelectorAll('canvas').forEach((cav) => {
+            cav.style.display = 'none'
+        })
+    })
 
-    crossbtn.disabled = false;
-    circlebtn.disabled = false;
-    resetbtn.style.display = "none";
-    empty.fill(true);
-    playerX = true;
-    rounds = 0;
-    enabled = true;
-    game.fill(0);
-    finished = false;
+    crossbtn.disabled = false
+    circlebtn.disabled = false
+    resetbtn.style.display = 'none'
+    empty.fill(true)
+    playerX = true
+    rounds = 0
+    enabled = true
+    game.fill(0)
+    finished = false
 
-    document.getElementById("hint").innerHTML = "Chose your turn &uparrow;";
-});
+    document.getElementById('hint').innerHTML = 'Chose your turn &uparrow;'
+})
 
 function show(block, index) {
     if (playerX) {
-        block.querySelector(".cross").style.display = "inline";
-        document.getElementById("hint").innerHTML = "Turn of" + `<span style="margin-top: -5px; margin-left: 18px">o</span>`;
-        game[index] = 1;
+        block.querySelector('.cross').style.display = 'inline'
+        document.getElementById('hint').innerHTML = 'Turn of' + `<span style="margin-top: -5px; margin-left: 18px">o</span>`
+        game[index] = 1
     } else {
-        block.querySelector(".circle").style.display = "inline";
-        document.getElementById("hint").innerHTML = "Turn of &times;";
-        game[index] = -1;
+        block.querySelector('.circle').style.display = 'inline'
+        document.getElementById('hint').innerHTML = 'Turn of &times;'
+        game[index] = -1
     }
-    block.style.backgroundColor = "hsl(210, 40%, 40%)";
-    empty[index] = false;
-    rounds += 1;
-    enabled = !enabled;
-    playerX = !playerX;
-    showResult();
+    block.style.backgroundColor = 'hsl(210, 40%, 40%)'
+    empty[index] = false
+    rounds += 1
+    enabled = !enabled
+    playerX = !playerX
+    showResult()
 
     if (finished || rounds == 9) {
-        resetbtn.style.display = "inline";
+        resetbtn.style.display = 'inline'
     }
 }
 
 function blockMouseover(block) {
-    block.style.backgroundColor = "hsl(210, 40%, 45%)";
+    block.style.backgroundColor = 'hsl(210, 40%, 45%)'
 }
 
 function blockMouseleave(block) {
-    block.style.backgroundColor = "hsl(210, 40%, 40%)";
+    block.style.backgroundColor = 'hsl(210, 40%, 40%)'
 }
 
 function algorithm() {
     if (rounds == 9 || finished) {
-        return -1;
+        return -1
     }
 
-    let index;
+    let index
     // do {
     // 	index = Math.floor(Math.random() * 9);
     // } while (empty[index] == false);
 
-    let tmpO;
+    let tmp
 
     switch (rounds) {
         case 1: // player: X, AI: O
-            break;
+            break
         case 2: // player: O, AI: X
-            tmpO = game.indexOf(-1);
-            if (tmpO == 0 || tmpO == 2) {
-                index = tmpO + 6;
-            } else if (tmpO == 3 || tmpO == 5) {
-                index = 4;
-            } else if (tmpO == 4) {
-                index = 0;
-            } else if (tmpO == 6 || tmpO == 8) {
-                index = tmpO - 6;
-            } else {
-                index = 6;
+            tmp = game.indexOf(-1)
+            if (tmp == 0 || tmp == 2) {
+                index = tmp + 6
+            } else if (tmp == 3 || tmp == 5) {
+                index = 4
+            } else if (tmp == 4) {
+                index = 0
+            } else if (tmp == 6 || tmp == 8) {
+                index = tmp - 6
+            } else { // tmp == 7
+                index = 6
             }
-            break;
+            break
         case 3:
-            break;
+            break
         case 4:
-            index = checkline(1);
-            break;
+            index = checkline(1)
+            if (index == -1) {
+                index = checkline(-1)
+            }
+            if (index == -1) {
+                tmp = game.indexOf(-1)
+                if (tmp == 0 || tmp == 2) {
+                    if (game[7] == 0) {
+                        index = 7
+                    } else {
+                        index = game[6] == 0 ? 6 : 8;
+                    }
+                } else if (tmp == 3 || tmp == 5 || tmp == 7) {
+                    index = 0
+                }
+            }
+            break
         case 5:
-            break;
+            break
         case 6:
-            break;
+            index = checkline(1)
+            if (index == -1) {
+                index = checkline(-1)
+            }
+            if (index == -1) {
+                tmp = game.indexOf(-1)
+                if (tmp == 0 || tmp == 2) {
+                    if (game[0] == -1 && game[2] == -1) {
+                        index = 4
+                    } else {
+                        index = tmp == 0 ? 2 : 0
+                    }
+                }
+            }
+            break
         case 7:
-            break;
+            break
         case 8:
-            index = game.indexOf(0);
-            break;
+            index = game.indexOf(0)
+            break
     }
 
-    return index;
+    return index
 }
 
 function showResult() {
-    let tmp;
+    let tmp
     for (const line of lines) {
-        tmp = 0;
+        tmp = 0
         for (let l = 0; l < 3; l++) {
-            tmp += game[line[l]];
+            tmp += game[line[l]]
         }
         if (tmp == 3 || tmp == -3) {
-            document.getElementById("hint").innerHTML = tmp > 0 ? "&times; win!" : `<span style="margin-top: -5px; margin-right: 18px;">o</span>` + " win!";
-            finished = true;
-            break;
+            document.getElementById('hint').innerHTML = tmp > 0 ? '&times; win!' : `<span style="margin-top: -5px; margin-right: 18px;">o</span>` + ' win!'
+            finished = true
+            break
         }
     }
 
     if (rounds == 9 && finished != true) {
-        document.getElementById("hint").innerHTML = "Draw!";
+        document.getElementById('hint').innerHTML = 'Draw!'
     }
 
     if (finished || rounds == 9) {
-        empty.fill(false);
+        empty.fill(false)
     }
 }
 
 function checkline(s) {
-    let index = -1;
+    let index = -1
     for (const line of lines) {
-        tmp = 0;
+        tmp = 0
         for (const l of line) {
-            tmp += game[l];
+            tmp += game[l]
         }
         if (tmp == 2 * s) {
             do {
-                index = game.indexOf(0, index + 1);
-            } while (!line.includes(index));
+                index = game.indexOf(0, index + 1)
+            } while (!line.includes(index))
 
-            return index;
-        } else if (tmp == 2 * -s) {
-            do {
-                index = game.indexOf(0, index + 1);
-            } while (!line.includes(index));
-
-            return index;
+            return index
         }
     }
 
-    return index;
+    return index
 }
